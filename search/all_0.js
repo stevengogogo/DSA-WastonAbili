a908059bbe4eb6b7f60156501816d1df190956bc
stevengogogo <stevengogogo@users.noreply.github.com>
2021-03-27 08:41:12 +0000

    deploy: 4143777456c211961aee868e65c03924fd7be73c

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cprojecttemplate_0',['CProjectTemplate',['../index.html',1,'']]]
+  ['double_20linked_20list_3a_20list_20migration_0',['Double Linked List: List migration',['../index.html',1,'']]]
 ];
