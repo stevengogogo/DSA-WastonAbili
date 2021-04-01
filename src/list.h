f398da8b4c8462d6abe1199de4d07547b41cb27d
stevengogogo <stevengogogo4321@gmail.com>
2021-04-01 18:14:11 +0800

    add Brief introduction to the method

--- src/list.h
+++ src/list.h
@@ -1,7 +1,7 @@
 /**
  * @file list.h
  * @author Shao-Ting Chiu
- * @brief Double linked list
+ * @brief Double linked list. This implementation provides O(1) complexity for reversing. See main page for details. This implementataion sacrifices the speed of reading. It required the check of the flag to next node  when moving from one node to another. However, HW1 P5 only needs to read the lines once. Noted that the direction of the next node is updated by XOR operation, so this double linked list is also known as XOR double linked list (https://en.wikipedia.org/wiki/XOR_linked_list).
  * @version 0.1
  * @date 2021-04-01
  * 
@@ -53,6 +53,12 @@ int iter_read(node*);
 /** get the flag point to the node. This function assumes a node is linked to at least one node */
 int get_flag2node(node* terminal_node);
 
+/**
+ * @brief Get the flag to reach NULL. The index is returned to get the node->ngb[index] = NULL.
+ * 
+ * @param terminal_node 
+ * @return int `2` for two NULL neigbhors, `0` and `1` for flags. 
+ */
 int get_flag2null(node* terminal_node);
 
 /* Return the direction to the dst. -1 for missing */
@@ -63,11 +69,25 @@ int get_flag2dst(node* src, node* dst);
 
 /** Return 1 if one element is pop; -1 for none */
 int leave(list*);
+
+/**
+ * @brief Insert new element
+ * 
+ * @param log new data
+ * @return int `1` for success adding; `-1` as error code
+ */
 int enter(list*, int log);
+
+/**
+ * @brief 
+ * 
+ * @param src source line
+ * @param dst destination line
+ */
 void migrate(list* src, list* dst);
 
 
-/** interface */
+/** interface. Use `stdin` for manipulating the linked lists */
 void interact_scanf(void);
 
 #endif
