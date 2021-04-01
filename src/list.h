6961676c86b85e06ac3ee0df2f961ddd4933af3e
stevengogogo <stevengogogo4321@gmail.com>
2021-04-01 17:58:50 +0800

    set comment for list h

--- src/list.h
+++ src/list.h
@@ -1,3 +1,14 @@
+/**
+ * @file list.h
+ * @author Shao-Ting Chiu
+ * @brief Double linked list
+ * @version 0.1
+ * @date 2021-04-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #ifndef LIST_H
 #define LIST_H
 
